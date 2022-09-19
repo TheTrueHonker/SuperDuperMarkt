cff22ff8540e55c57e56be6741a92b09d12722cd
Daniel Wemmer <daniel.wemmer.dw@gmail.com>
2022-09-19 16:55:48 +0200

    feat: initial commit
     - Add Source Project
     - Add Test Project

---
+++ SuperDuperMarkt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuperDuperMarkt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
