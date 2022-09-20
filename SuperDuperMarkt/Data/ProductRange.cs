f52be983942a97d2e4ee34936dafbf23e622e9e1
Daniel Wemmer <daniel.wemmer.dw@gmail.com>
2022-09-20 17:45:44 +0200

    feat: Add ProductRange object
     - Add Singleton ProductRange object
     - Add demo output and usage in Program.cs

---
+++ SuperDuperMarkt/Data/ProductRange.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SuperDuperMarkt.Data.Products;
+
+namespace SuperDuperMarkt.Data
+{
+    public class ProductRange
+    {
+        private static ProductRange productRange;
+        private List<Product> range;
+
+        private ProductRange()
+        {
+            range = new List<Product>();
+        }
+
+        public static ProductRange GetProductRange()
+        {
+            if (productRange == null)
+                productRange = new ProductRange();
+            return productRange;
+        }
+
+        public void PrintRangeForDate(DateTime dateTime)
+        {
+            Console.WriteLine($"Range for {dateTime:dd.MM.yyyy}");
+            Console.WriteLine("---------------------");
+            foreach(Product product in range)
+            {
+                product.UpdateQuality(dateTime);
+                if(!product.IsQualityGood())
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(product.ToString());
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                } else
+                {
+                    Console.WriteLine(product.ToString());
+                }
+            }
+            Console.WriteLine("");
+        }
+
+        public void ImportRangeList(List<Product> newProducts)
+        {
+            range = newProducts;
+        }
+    }
+}
--- SuperDuperMarkt/Program.cs
+++ SuperDuperMarkt/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SuperDuperMarkt.Data;
+using SuperDuperMarkt.Data.Products;
+using System;
+using System.Collections.Generic;
 
 namespace SuperDuperMarkt
 {
@@ -6,7 +9,20 @@ namespace SuperDuperMarkt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var products = new List<Product>
+            {
+                new Cheese("Cheddar", 1.76f, 31, 50),
+                new Wine("Red Wine", 46f, 1)
+            };
+
+            var range = ProductRange.GetProductRange();
+            range.ImportRangeList(products);
+            range.PrintRangeForDate(DateTime.Now);
+            for(int i = 1; i <= 5; i++)
+            {
+                range.PrintRangeForDate(DateTime.Now.AddDays(i));
+            }
+            
         }
     }
 }
