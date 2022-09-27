4950403f6c2dc7c1241f08b118718f7e4215a6f2
Daniel Wemmer <daniel.wemmer.dw@gmail.com>
2022-09-27 18:42:32 +0200

    feat: add product "Bread"

---
+++ SuperDuperMarkt/Data/Products/Bread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperDuperMarkt.Data.Products
+{
+    public class Bread: Product
+    {
+        private const int DAILY_QUALITY_MODIFIER = 2;
+
+        public Bread(string description, float fixPrice, int quality, DateTime dueDate, DateTime dateCreated)
+            :base(description, quality,dueDate,fixPrice,DAILY_QUALITY_MODIFIER,dateCreated)
+        {
+        }
+
+        public Bread(string description, float fixPrice, int quality, DateTime dueDate)
+            : base(description, quality, dueDate, fixPrice, DAILY_QUALITY_MODIFIER)
+        {
+        }
+
+        public override bool IsQualityGood()
+        {
+            if (Quality > 0)
+                return true;
+            return false;
+        }
+    }
+}
