9d3c2dd09e63eabf76c5d6c81c498c8d26f97901
Daniel Wemmer <daniel.wemmer.dw@gmail.com>
2022-09-27 19:46:19 +0200

    fix: decimal values in CSV
    Decimal values in CSV files are now read correctly

--- SuperDuperMarkt/Data/ProductImports/CSVImporter.cs
+++ SuperDuperMarkt/Data/ProductImports/CSVImporter.cs
@@ -10,6 +10,7 @@ namespace SuperDuperMarkt.Data.ProductImports
     class CSVImporter : IProductImport
     {
         private const string CSV_HEADER = "Type,Description,Quality,DueDate,DueInDays,FixPrice,DailyQualityModifier,DateCreated";
+        private const string ASSEMBLY_CLASS_NAME = "SuperDuperMarkt.Data.Products.{0}, SuperDuperMarkt";
 
         private string path;
 
@@ -54,39 +55,62 @@ namespace SuperDuperMarkt.Data.ProductImports
 
         private Product ConvertLineToProduct(string line)
         {
-            var parameters = line.Split(',');
+            var parameters = GetLineParameters(line);
+            var description = parameters.GetValueOrDefault(CSV_Parameter.Description, "");
+            float.TryParse(parameters.GetValueOrDefault(CSV_Parameter.FixPrice, "0"), out var fixPrice);
+            int.TryParse(parameters.GetValueOrDefault(CSV_Parameter.Quality), out var quality);
+            int.TryParse(parameters.GetValueOrDefault(CSV_Parameter.DueInDays), out var dueInDays);
+            DateTime.TryParse(parameters.GetValueOrDefault(CSV_Parameter.DueDate), out var dueDate);
             DateTime dateCreated = DateTime.Now;
-            switch(parameters[(int)CSV_Parameters.Type])
+            if (parameters.GetValueOrDefault(CSV_Parameter.DateCreated,"") != "")
+            {
+                dateCreated = DateTime.Parse(parameters.GetValueOrDefault(CSV_Parameter.DateCreated));
+            }
+            switch (parameters[(int)CSV_Parameter.Type])
             {
                 case "Cheese":
-                    if(parameters[(int)CSV_Parameters.DateCreated] != "")
-                    {
-                        dateCreated = DateTime.Parse(parameters[(int)CSV_Parameters.DateCreated]);
-                    }
-                    return new Cheese(
-                        parameters[((int)CSV_Parameters.Description)],
-                        ((float)double.Parse(parameters[(int)CSV_Parameters.FixPrice], CultureInfo.InvariantCulture)),
-                        int.Parse(parameters[(int)CSV_Parameters.Quality]),
-                        int.Parse(parameters[(int)CSV_Parameters.DueInDays]),
-                        dateCreated);
+                    return new Cheese(description, fixPrice, quality, dueInDays, dateCreated);
                 case "Wine":
-                    if (parameters[((int)CSV_Parameters.DateCreated)] != "")
-                    {
-                        dateCreated = DateTime.Parse(parameters[(int)CSV_Parameters.DateCreated]);
-                    }
-                    return new Wine(
-                        parameters[((int)CSV_Parameters.Description)],
-                        ((float)double.Parse(parameters[(int)CSV_Parameters.FixPrice], CultureInfo.InvariantCulture)),
-                        int.Parse(parameters[(int)CSV_Parameters.Quality]),
-                        dateCreated);
+                    return new Wine(description, fixPrice, quality, dateCreated);
+                case "Bread":
+                    return new Bread(description, fixPrice, quality, dueDate, dateCreated);
                 default:
-                    Console.WriteLine($"Unsupported product: {parameters[(int)CSV_Parameters.Type]}");
+                    Console.WriteLine($"Unsupported product: {parameters.GetValueOrDefault(CSV_Parameter.Type)}");
                     return null;
             }
         }
+
+        private Dictionary<CSV_Parameter, string> GetLineParameters(string line)
+        {
+            var dict = new Dictionary<CSV_Parameter, string>();
+            string parameter = "";
+            int currentParameter = 0;
+            bool ignoreComma = false;
+            foreach(char character in line)
+            {
+                if (character == '"')
+                {
+                    ignoreComma = !ignoreComma;
+                }
+                else
+                {
+                    if (character == ',' && !ignoreComma)
+                    {
+                        dict.Add((CSV_Parameter)currentParameter, parameter);
+                        parameter = "";
+                        currentParameter++;
+                    }
+                    else
+                    {
+                        parameter += character;
+                    }
+                }
+            }
+            return dict;
+        }
     }
 
-    enum CSV_Parameters{
+    enum CSV_Parameter{
         Type = 0,
         Description = 1,
         Quality = 2,
